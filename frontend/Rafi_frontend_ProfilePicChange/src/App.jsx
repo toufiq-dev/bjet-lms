1b6bb23207c8fd337480b2a32de638bed56ec1a3
Rafi Hasan <rafihasanmmsc@gmail.com>
2024-07-13 11:52:23 +0600

    Revert "Deleted folder Rafi_frontend_ProfilePicChange"
    This reverts commit 3a336c62c03a867da133cbc185590654832857dd.

---
+++ frontend/Rafi_frontend_ProfilePicChange/src/App.jsx
@@ -0,0 +1,22 @@
+// src/App.jsx
+import React from 'react';
+import ProfilePage from './ProfilePage';
+import './App.css';
+import ButtonExample from './inputs/ButtonExample';
+
+function App() {
+  //console.log("App component rendered");
+  return (
+    //<div >
+    //<ProfilePage />
+    //<ButtonExample/>
+    //</div>
+    <div>
+    
+      <ProfilePage/>
+
+    </div>
+  );
+}
+
+export default App;
