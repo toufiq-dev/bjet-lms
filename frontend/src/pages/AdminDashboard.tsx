d38ab423d36295c4a5ae99d32e307b23ab8b1b24
nayem73 <nayem73@gmail.com>
2024-07-14 10:57:14 +0600

    Integrated the changes regarding user creation into the AdminDashboard

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import UserCreationModal from "../components/modals/UserCreationModal";
 import ResponsiveDrawer from "../components/drawers/ResponsiveDrawer";
 import Toolbar from "@mui/material/Toolbar";
+import SignOutButton from "../components/buttons/SignOutButton";
 
 const drawerWidth = 150;
 
 const AdminDashboard = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [userType, setUserType] = useState<"student" | "teacher">("student"); // Default to student
+
+  const handleStudentCreation = () => {
+    setModalOpen(true);
+    setUserType("student");
+  };
+
+  const handleTeacherCreation = () => {
+    setModalOpen(true);
+    setUserType("teacher");
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
   return (
     <Box display="flex">
       <ResponsiveDrawer title="Dashboard" drawerWidth={drawerWidth} />
@@ -18,7 +38,18 @@ const AdminDashboard = () => {
         }}
       >
         <Toolbar />
-        <UserCreationModal />
+        <SignOutButton />
+        <UserCreationModal
+          open={modalOpen}
+          onClose={handleCloseModal}
+          userType={userType}
+        />
+        <Button variant="contained" onClick={handleStudentCreation} sx={{ mr: 2 }}>
+          Create Student
+        </Button>
+        <Button variant="contained" onClick={handleTeacherCreation}>
+          Create Teacher
+        </Button>
       </Box>
     </Box>
   );
